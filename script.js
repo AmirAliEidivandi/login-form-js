64ced10fc467411b48fdc23bc5f12b903aed6847
AmirAli6578 <amirah648887@gmail.com>
2022-01-19 02:04:54 +0330

    19/1/2022
    change js and css files

--- script.js
+++ script.js
@@ -1,55 +1,71 @@
 const form = document.getElementById("form");
-const userName = document.getElementById("username");
+const username = document.getElementById("username");
 const email = document.getElementById("email");
 const password = document.getElementById("password");
 const password2 = document.getElementById("password2");
 
-form.addEventListener('sumbit', e => {
-    e.preventDefault()
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
 
-    validateInputs()
-})
+    validateInputs();
+});
 
-const setError = (elemnt, message) => {
-    const inputControl = elemnt.parentElement;
-    const errorDispaly = inputControl.querySelector('.error')
+const setError = (element, message) => {
+    const inputControl = element.parentElement;
+    const errorDisplay = inputControl.querySelector(".error");
 
-    errorDispaly.innerText = message;
-    inputControl.classList.add('error')
-    inputControl.classList.remove('success')
-}
+    errorDisplay.innerText = message;
+    inputControl.classList.add("error");
+    inputControl.classList.remove("success");
+};
 
-const setSuccess = element => {
+const setSuccess = (element) => {
     const inputControl = element.parentElement;
     const errorDisplay = inputControl.querySelector(".error");
 
     errorDisplay.innerText = "";
     inputControl.classList.add("success");
     inputControl.classList.remove("error");
-}
+};
 
-const isValidEmail = email => {
+const isValidEmail = (email) => {
     const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    return re.test(String(email).toLowerCase())
-}
+    return re.test(String(email).toLowerCase());
+};
 
 const validateInputs = () => {
-    const userNameValue = userName.value.trim()
-    const emailValue = email.value.trim()
-    const passowrdValue = password.value.trim()
-    const passowrd2Value = password2.value.trim()
+    const usernameValue = username.value.trim();
+    const emailValue = email.value.trim();
+    const passwordValue = password.value.trim();
+    const password2Value = password2.value.trim();
 
-    if (userNameValue === '') {
-        setError(userName, 'Username is required')
+    if (usernameValue === "") {
+        setError(username, "Username is required");
     } else {
-        setSuccess(userName)
+        setSuccess(username);
     }
 
-    if (emailValue === '') {
-        setError(email, 'Email is required')
+    if (emailValue === "") {
+        setError(email, "Email is required");
     } else if (!isValidEmail(emailValue)) {
-        setError(email, 'Provitde a valid email address')
+        setError(email, "Provide a valid email address");
     } else {
-        setSuccess(email)
+        setSuccess(email);
     }
-}
+
+    if (passwordValue === "") {
+        setError(password, "Password is required");
+    } else if (passwordValue.length < 8) {
+        setError(password, "Password must be at least 8 character.");
+    } else {
+        setSuccess(password);
+    }
+
+    if (password2Value === "") {
+        setError(password2, "Please confirm your password");
+    } else if (password2Value !== passwordValue) {
+        setError(password2, "Passwords doesn't match");
+    } else {
+        setSuccess(password2);
+    }
+};
